9b2f32547201a6223109ea389c76ab1779707bd8
Sarvesh Khire <sarveshkhire@gmail.com>
2023-04-10 07:11:57 +0000

    commit

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,15 +1,15 @@
-order_payments as (
+with order_payments as (
     select
         order_id,
-        sum(case when status = 'success' then amount end) as amount
+        sum(case when status_ = 'success' then amount end) as amount
     from {{ ref('stg_payments') }}
     group by 1
-),
+)
 select 
     o.order_id as order_id,
-    p.customer_id as customer_id,
-    o.order_date
-    coalesce(o.amount, 0) as amount 
+    o.customer_id as customer_id,
+    o.order_date,
+    coalesce(p.amount, 0) as amount 
 from {{ ref('stg_orders') }} as o
 left join order_payments as p  
     on (o.order_id = p.order_id)
